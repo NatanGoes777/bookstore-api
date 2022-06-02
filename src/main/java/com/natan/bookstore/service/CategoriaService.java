817301889cd6db7ab9ba44b25ba47d97f2123944
NatanGoes777 <natan.goes777@gmail.com>
2022-06-02 20:31:11 -0300

    findById para Categoria

---
+++ src/main/java/com/natan/bookstore/service/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.natan.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.natan.bookstore.domain.Categoria;
+import com.natan.bookstore.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = categoriaRepository.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
