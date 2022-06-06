ecf421b0b2d857597c4283309b94a0f5fcb5bd91
NatanGoes777 <natan.goes777@gmail.com>
2022-06-06 18:18:23 -0300

    exceção personalizada para objeto não encontrado

--- src/main/java/com/natan/bookstore/service/CategoriaService.java
+++ src/main/java/com/natan/bookstore/service/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.natan.bookstore.domain.Categoria;
 import com.natan.bookstore.repositories.CategoriaRepository;
+import com.natan.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não Encontrado! Id: " + id + ", tipo: " + Categoria.class.getName()));
 	}
-	
+
 }
